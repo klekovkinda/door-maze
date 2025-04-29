364e59fbc8b520741bce095bec903c398be686dd
Dmitrii Klekovkin <dmitry.klekovkin@tis.biz>
2025-04-29 09:16:24 +0200

    add hero

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ TILE_SIZE = 100
 assets = {"flor": pygame.image.load("assets/flor.png"),
           "wall": pygame.image.load("assets/wall.png"), }
 
+hero = {"x": 0, "y": 0}
+hero_image = pygame.image.load("assets/hero.png")
+
 
 def draw_map():
     for row_index, row in enumerate(map_config["rows"]):
@@ -22,6 +25,10 @@ def draw_map():
             rotation = tile["rotation"]
             rotated_tile = pygame.transform.rotate(assets[tile_type], rotation)
             screen.blit(rotated_tile, (x, y))
+    # Draw the hero
+    hero_x = hero["x"] * TILE_SIZE
+    hero_y = hero["y"] * TILE_SIZE
+    screen.blit(hero_image, (hero_x, hero_y))
 
 
 running = True
@@ -30,6 +37,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                hero["y"] -= 1  #
+            elif event.key == pygame.K_DOWN:
+                hero["y"] += 1
+            elif event.key == pygame.K_LEFT:
+                hero["x"] -= 1
+            elif event.key == pygame.K_RIGHT:
+                hero["x"] += 1
 
     screen.fill((0, 0, 0))
     draw_map()
